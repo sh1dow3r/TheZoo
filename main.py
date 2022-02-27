a5395c38114f7b34acf93085e707638fd7ad747c
sh1dow3r <sh1dow3r@gmail.com>
2022-02-27 21:43:09 +0300

    including python script to take care of vars generation and project setup

---
+++ main.py
@@ -0,0 +1,95 @@
+import random, string, configparser
+
+
+def password_gen(length):
+    #ref https://medium.com/analytics-vidhya/create-a-random-password-generator-using-python-2fea485e9da9
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    num = string.digits
+    #symbols = string.punctuation
+    password_space = lower + upper + num #+ symbols
+    temp_pass = random.sample(password_space,length)
+    password = "".join(temp_pass)
+    return password
+
+
+def write_line(file,key,val):
+    pass
+
+def setup_mwdb_vars():
+    ADMIN_PASSWORD=repr(password_gen(32))
+    SECRET_KEY=repr(password_gen(32))
+    POSTGRES_PASSWORD=repr(password_gen(32))
+    MINIO_ACCESS_KEY=repr(password_gen(32))
+    MINIO_SECRET_KEY=repr(password_gen(32))
+    #MINIO_ROOT_USER=root
+    #MINIO_ROOT_PASSWORD=
+    filename = "mwdb-vars.env"
+    fd = open(filename ,"w")
+    fd.truncate() #flush content before writing
+    mwdb_vars_setup="MWDB_ADMIN_LOGIN=admin\n\
+ADMIN_PASSWORD={}\n\
+SECRET_KEY={}\n\
+POSTGRES_PASSWORD={}\n\
+MINIO_ACCESS_KEY={}\n\
+MINIO_SECRET_KEY={}\n\
+MWDB_REDIS_URI=redis://redis/\n\
+MWDB_POSTGRES_URI=postgresql://mwdb:{}@postgres/mwdb".format(ADMIN_PASSWORD,SECRET_KEY,POSTGRES_PASSWORD,MINIO_ACCESS_KEY,MINIO_SECRET_KEY,POSTGRES_PASSWORD)
+
+    mwdb_opt_vars_setup="MWDB_BASE_URL=http://127.0.0.1\n\
+MWDB_ADMIN_EMAIL=admin@localhost\n\
+MWDB_BASE_URL=http://127.0.0.1\n\
+MWDB_MAIL_SMTP=localhost:25\n\
+MWDB_MAIL_FROM=noreply@mwdb.dev\n\
+MWDB_RECAPTCHA_SITE_KEY=6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI\n\
+MWDB_RECAPTCHA_SECRET=6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe\n\
+MINIO_ACCESS_KEY={}\n\
+MINIO_SECRET_KEY={}\n\
+UWSGI_PROCESSES=4\n\
+POSTGRES_USER=mwdb\n\
+POSTGRES_DB=mwdb\n\
+POSTGRES_PASSWORD={}\n\
+MWDB_ENABLE_RATE_LIMIT=1\n\
+MWDB_ENABLE_REGISTRATION=1\n\
+REACT_APP_API_URL=/api/\n\
+HOST=0.0.0.0".format(ADMIN_PASSWORD,MINIO_ACCESS_KEY,MINIO_SECRET_KEY,POSTGRES_PASSWORD)
+    fd.write(mwdb_vars_setup)
+    fd.write(mwdb_opt_vars_setup)
+    fd.close()
+
+    #install dependencies 
+    deb = "itsdangerous==2.0.1 "
+    karton_dashboard = "./karton_plugins/karton-dashboard/requirements.txt"
+    karton_reporter = "./karton_plugins/karton-mwdb-reporter/requirements.txt"
+    fd = open(karton_dashboard ,"w")
+    fd.write(deb)
+    fd.close()
+    fd = open(karton_reporter ,"w")
+    fd.write(deb)
+    fd.close()
+    
+    #setup ini for karton and mwdb-core communitcation
+    karton_ini = "./TheZoo_volume/karton/karton.ini" 
+    mwdb_ini = "./TheZoo_volume/karton/mwdb.ini"
+    setup_ini_file(karton_ini, "mwdb", "password", ADMIN_PASSWORD)
+    setup_ini_file(karton_ini, "minio", "access_key", MINIO_ACCESS_KEY)
+    setup_ini_file(karton_ini, "minio", "secret_key", MINIO_SECRET_KEY)
+    setup_ini_file(mwdb_ini, "mwdb", "secret_key", SECRET_KEY)
+    setup_ini_file(mwdb_ini, "mwdb", "s3_storage_access_key", MINIO_ACCESS_KEY)
+    setup_ini_file(mwdb_ini, "mwdb", "s3_storage_secret_key", MINIO_SECRET_KEY)
+    
+    
+def setup_ini_file(ini_file, section, key, val):
+    config = configparser.ConfigParser()
+    config.set(section,key,val) 
+    with open(ini_file, 'w') as configfile:
+        config.write(configfile)
+
+
+def main():
+    setup_mwdb_vars()
+
+if __name__ == '__main__':
+    main()
+
+
